278bcec1f15dd9afb387008927603df44d2ace65
Kevin Hyde <kphyde@gmail.com>
2024-02-19 18:54:36 -0800

    Add single user password from env

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,18 +2,25 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from app.utils import create_access_token
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 router = APIRouter()
 
 def authenticate_user(username: str, password: str):
-    # // Here, you should verify the username and password, e.g., against a database
-    # // For simplicity, let's assume the user is authenticated
-    user = {"username": username, "password": password}  # // This should be your actual user model or dictionary
-    if not user:
+    # There is only one user in this example
+    env_username = os.getenv("USERNAME")
+    env_password = os.getenv("PASSWORD")
+
+    if username == env_username and password == env_password:
+        user = {"username": username, "password": password}
+        return user
+    else:
         return False
-    return user
 
 @router.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
--- app/routers/auth_test.py
+++ app/routers/auth_test.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from typing import Optional
@@ -9,10 +9,6 @@ load_dotenv()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-status = {
-    "HTTP_401_UNAUTHORIZED": 401
-}
-
 router = APIRouter()
 
 @router.get("/items/")
@@ -21,7 +17,7 @@ async def read_items():
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
-        status_code=status["HTTP_401_UNAUTHORIZED"],
+        status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
