cfd64031b3cdd48aa4893f1d52af46a546c21b31
Kevin Hyde <kphyde@gmail.com>
2024-02-19 19:35:53 -0800

    add v1 versioning to all routes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter 
 from app.routers import items, users, grimoire, auth_test, auth
 
 app = FastAPI()
+v1_router = APIRouter(prefix="/v1")
 
-app.include_router(items.router)
-app.include_router(users.router)
-app.include_router(grimoire.router)
-app.include_router(auth_test.router)
-app.include_router(auth.router)
+v1_router.include_router(items.router)
+v1_router.include_router(users.router)
+v1_router.include_router(grimoire.router)
+v1_router.include_router(auth_test.router)
+v1_router.include_router(auth.router)
+
+app.include_router(v1_router)
